475533bb65044202d020ed8fc73bd63c85766a5a
Akhtam Ismatov <akhtam.ismatov@gmail.com>
2018-04-12 15:11:10 -0700

    jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,14 +6,25 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div className="container">
+      <div>
         <div className="banner">
-          <div className="main-text">
-            <h1>SHARE YOUR STYLE #UNIQLYFEWEAR</h1>
+          <h1>SHARE YOUR STYLE #UNIQLYFEWEAR</h1>
+          <div className="addphoto">
+            <a href="#">Add Photo</a>
+          </div>
+        </div>
+        <div className="slider-body">
+          <div className="slider-box">
+            <ul className="slider">
+              <li><img src="pic1.jpeg" /></li>
+              <li><img src="pic2.jpeg" /></li>
+              <li><img src="pic3.jpeg" /></li>
+              <li><img src="pic4.jpeg" /></li>
+            </ul>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
